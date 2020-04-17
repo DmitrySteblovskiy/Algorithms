f927731de99d94916ff55f4b8bd94ac01df37b21
DmitrySteblovskiy <60884905+DmitrySteblovskiy@users.noreply.github.com>
2020-04-17 20:38:41 +0300

    Updating Task_A

--- Task_A/Task_A.cpp
+++ Task_A/Task_A.cpp
@@ -1,40 +1,37 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
 #include <math.h>
 #include <queue>
-#include <algorithm>
+#include <vector>
+
 
 
 void dfs(const std::vector <std::vector <int>> &graph, std::vector<int> &answer, std::vector<int> &used, int node, bool &cycle_checker, std::vector<bool> &blocked) {
 
 	used[node] = 1;
 
+	for (size_t i = 0; i < graph[node].size(); ++i) {
 
-	for (int i = 0; i < graph[node].size(); ++i) {
-
-		if ((used[graph[node][i]] == 1) && (blocked[graph[node][i]] == false)) {
-
+		if ((used[graph[node][i]]) && (blocked[graph[node][i]] == false)) {
 			cycle_checker = true;
 			return;
 		}
-
+		
 		if (blocked[graph[node][i]] == false) {
-
 			dfs(graph, answer, used, graph[node][i], cycle_checker, blocked);
 		}
 	}
-
-
 	blocked[node] = true;
 	answer.push_back(node);
 }
 
 void inversion(std::vector<int> &answer) {
 
-	for (int i = 0; i < (answer.size() / 2); ++i) {
+	for (size_t i = 0; i < (answer.size() / 2); ++i) {
 		std::swap(answer[i], answer[answer.size() - 1 - i]);
 	}
 }
+
 
 int main() {
 
@@ -48,19 +45,15 @@ int main() {
 	std::vector<int> answer;
 	std::vector<int> used(n, 0);
 
-
 	for (int i = 0; i < m; ++i) {
-
 		std::cin >> a >> b;
 		graph[a].push_back(b);
 		traversed[b] = true;
 	}
 
-
 	for (int i = 0; i < n; ++i) {
 		std::sort(graph[i].begin(), graph[i].end());
 	}
-
 
 	for (int i = 0; i < n && not cycle_checker; ++i) {
 		if (traversed[i] == 0) {
@@ -68,13 +61,11 @@ int main() {
 		}
 	}
 
-
 	inversion(answer);
 
-
-	if ((cycle_checker == 0) && (answer.empty() == 0)) {
+	if ((!cycle_checker) && (answer.empty() == 0)) {
 		std::cout << "YES" << std::endl;
-		for (int i = 0; i < answer.size(); ++i) {
+		for (size_t i = 0; i < answer.size(); ++i) {
 			std::cout << answer[i] << " ";
 		}
 	}
