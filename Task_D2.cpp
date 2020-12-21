d2bce0b9ad1b160822bbe05f51ce76f1c04f0adc
DmitrySteblovskiy <60884905+DmitrySteblovskiy@users.noreply.github.com>
2020-12-21 15:32:44 +0300

    Fixing mistakes in D.cpp
    Мы же при копировании память зря потратим, ну ладно(

--- Task_D2.cpp
+++ Task_D2.cpp
@@ -14,10 +14,9 @@
 // S(u; v) = Xu2+2Yuv + Zv2 для всех u; v > 0, причем коэффициент X всегда равен площади S(A), а Z — площади B.
 // А вот коэффициент Y — так называемую смешанную площадь S(A; B) выпуклых фигур A и B — Арсений подсчитать не мог.
 // А вы сможете? Да.
-// https://contest.yandex.ru/contest/20642/run-report/42341745/
+// https://contest.yandex.ru/contest/20642/run-report/45735856/
 
-
-const long double min_e = 0.00000000001;
+const long double MIN_E = 0.00000000001;
 
 class Vector2D {    // класс по сути взят из задачи А1
 public:
@@ -25,11 +24,12 @@ public:
     long double x;
     long double y;
  
-    Vector2D(const Vector2D &other)             = default;
+    Vector2D(const Vector2D &other) = default;
     Vector2D &operator = (const Vector2D &other) = default;
-    Vector2D(Vector2D && other)                  = default;
-    Vector2D &operator = (Vector2D && other)      = default;
-    ~Vector2D()                                 = default;
+    Vector2D(Vector2D && other) = default;
+    Vector2D &operator = (Vector2D && other) = default;
+    Vector2D()= default;
+    ~Vector2D() = default;
  
     Vector2D operator - () {
         return Vector2D(-x, -y);
@@ -54,16 +54,46 @@ public:
     friend long double Dot(const Vector2D &v1, const Vector2D &v2) {
         return v1.x * v2.x + v1.y * v2.y;
     }
+
+	friend std::istream& operator >> (std::istream &, Vector2D&);
 };
- 
- 
-bool LessPolarAngle(Vector2D v1, Vector2D v2) {     // сортировка
-    bool less_angle = (v1.x * v2.y - v1.y * v2.x > 0);  // я правильно понимаю, что по google codestyle лучше объединить это с ретурном независимо от размера строки?
+
+std::istream& operator >> (std::istream &in, Vector2D &vec) {
+    in >> vec.x;
+    in >> vec.y;
+
+    return in;
+}
+
+bool LessPolarAngle(const Vector2D &v1, const Vector2D &v2) {     // сортировка
+    bool less_angle = (v1.x * v2.y - v1.y * v2.x > 0);
     return less_angle;
 }
- 
- 
-std::vector<Vector2D> MinkowskiSum(std::vector<Vector2D> &polygon1, std::vector<Vector2D> &polygon2) {      // поиск суммы Минковского с использованием сортировки
+
+std::vector<Vector2D> Normalize(const std::vector<Vector2D> &v) {
+    std::vector<Vector2D> r;
+    size_t min = 0;
+
+    for (size_t i = 1; i < v.size(); ++i) {
+        if (v[i].x < v[min].x || (std::abs(v[i].x - v[min].x) < MIN_E && v[i].y < v[min].y)) {
+            min = i;
+        }
+    }
+
+    for (size_t i = 0; i < v.size(); ++i) {
+        r.push_back(v[(i + min) % v.size()]);
+    }
+
+    return r;
+}
+
+std::vector<Vector2D> MinkowskiSum(const std::vector<Vector2D> &polygon_1, const std::vector<Vector2D> &polygon_2) {      // поиск суммы Минковского с использованием сортировки
+    std::vector<Vector2D> polygon1 = polygon_1;
+    std::vector<Vector2D> polygon2 = polygon_2;
+
+    polygon1 = Normalize(polygon1);
+    polygon2 = Normalize(polygon2);
+
     polygon1.push_back(polygon1[0]);
     polygon2.push_back(polygon2[0]);
     polygon1.push_back(polygon1[1]);
@@ -89,26 +119,7 @@ std::vector<Vector2D> MinkowskiSum(std::vector<Vector2D> &polygon1, std::vector<
     return result;
 }
 
-
-std::vector<Vector2D> Normalize(const std::vector<Vector2D> &v) {
-    std::vector<Vector2D> r;
-    size_t min = 0;
-
-    for (size_t i = 1; i < v.size(); ++i) {
-        if (v[i].x < v[min].x || (std::abs(v[i].x - v[min].x) < min_e && v[i].y < v[min].y)) {
-            min = i;
-        }
-    }
- 
-    for (size_t i = 0; i < v.size(); ++i) {
-        r.push_back(v[(i + min) % v.size()]);
-    }
-
-    return r;
-}
-
-
-long double Gauss(std::vector<Vector2D> polygon) {      // с помощью формулы Гаусса считаем площади выпуклых многоугольников, используя лишь координаты их вершин
+long double Gauss(const std::vector<Vector2D> &polygon) {      // с помощью формулы Гаусса считаем площади выпуклых многоугольников, используя лишь координаты их вершин
     long double answer = 0;
     size_t vec_size = polygon.size();
 
@@ -132,20 +143,17 @@ int main() {
 
     std::cin >> n;
     for (size_t i = 0; i < n; ++i) {
-        long double x, y;
-        std::cin >> x >> y;
-        polygon1.emplace_back(x, y);
+        Vector2D vec;
+        std::cin >> vec;
+        polygon1.push_back(vec);
     }
 
     std::cin >> m;
     for (size_t i = 0; i < m; ++i) {
-        long double x, y;
-        std::cin >> x >> y;
-        polygon2.emplace_back(x, y);
+        Vector2D vec;
+        std::cin >> vec;
+        polygon2.push_back(vec);
     }
-
-    polygon1 = Normalize(polygon1);
-    polygon2 = Normalize(polygon2);
 
     std::vector<Vector2D> msum = MinkowskiSum(polygon1, polygon2);
     answer = 0.5 * (Gauss(msum) - Gauss(polygon1) - Gauss(polygon2));   // считаем смешанную (искомую) площадь
